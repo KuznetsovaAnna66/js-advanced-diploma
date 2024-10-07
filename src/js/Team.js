543d7c14b376ba18e69cd8eb70b070aaff56d305
Anna Kuznetsova <ann_kuzn@mail.ru>
2024-10-07 20:47:53 +0500

    initial commit - first part of the task

--- src/js/Team.js
+++ src/js/Team.js
@@ -13,4 +13,28 @@
  * */
 export default class Team {
   // TODO: write your logic here
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error("Такой персонаж уже добавлен");
+    }
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => this.members.add(character));
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+
+  *[Symbol.iterator]() {
+    for (const character of [...this.characters]) {
+      yield character;
+    }
+  }
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -24,17 +24,36 @@
  * */
 export function calcTileType(index, boardSize) {
   // TODO: ваш код будет тут
-  return 'center';
+
+  const row = Math.floor(index / boardSize);
+  const col = index % boardSize;
+
+  if (row === 0) {
+    if (col === 0) return "top-left";
+    if (col === boardSize - 1) return "top-right";
+    return "top";
+  }
+
+  if (row === boardSize - 1) {
+    if (col === 0) return "bottom-left";
+    if (col === boardSize - 1) return "bottom-right";
+    return "bottom";
+  } else {
+    if (col === 0) return "left";
+    if (col === boardSize - 1) return "right";
+  }
+
+  return "center";
 }
 
 export function calcHealthLevel(health) {
   if (health < 15) {
-    return 'critical';
+    return "critical";
   }
 
   if (health < 50) {
-    return 'normal';
+    return "normal";
   }
 
-  return 'high';
+  return "high";
 }
